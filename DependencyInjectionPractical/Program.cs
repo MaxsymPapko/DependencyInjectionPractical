7a2cb7446dfb4d55c412284afaca2dd5fab12aca
Maxim Papko <papkom50@gmail.com>
2025-04-28 11:11:31 +0300

    made a basic DI logic using a constructor

--- DependencyInjectionPractical/Program.cs
+++ DependencyInjectionPractical/Program.cs
@@ -1,3 +1,38 @@
-﻿
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ILoger logger = new ConsoleLoger();
+        Application app = new Application(logger);
+        app.Run();
+    }
+}
 
-Console.WriteLine("Hello, World!");
+public interface ILoger
+{
+    void Log(string message);
+}
+
+public class ConsoleLoger : ILoger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine($"[LOG]:{message}");
+    }
+}
+
+public class Application
+{
+    private readonly ILoger _loger;
+
+    public Application(ILoger loger)
+    {
+        _loger = loger;
+    }
+
+    public void Run()
+    {
+        _loger.Log("App is running");
+    }
+}
